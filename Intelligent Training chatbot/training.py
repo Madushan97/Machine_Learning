779f4c13b43aa165339d739fcf7a898b03568a8b
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-06-21 21:17:29 +0530

    avoiding duplications

--- Intelligent Training chatbot/training.py
+++ Intelligent Training chatbot/training.py
@@ -3,7 +3,7 @@ import json
 import pickle
 
 import nltk
-nltk.download('punkt')
+
 import numpy as np
 
 from nltk.stem import WordNetLemmatizer       # wor, works, working, worked are same
@@ -12,7 +12,7 @@ from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 # reading the json content
 intents = json.loads(open('intents.json').read())
@@ -27,10 +27,19 @@ ignore_latters = ['?', '!', '.', ',']
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)              # I am john -> split the sentence into tokens
-        words.append(word_list)                         # word_list into words list
+        words.extend(word_list)                              # word_list into words list
         documents.append((word_list, intent['tag']))
-        # checking class is whether in the class list or not
-        if intent['tag'] not in classes:
+        if intent['tag'] not in classes:                     # checking class is whether in the class list or not
             classes.append(intent['tag'])
 
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_latters]
+words = sorted(set(words))                                  # eliminate the duplicate
+
+# avoiding duplicates in classes
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(words, open('classes.pkl', 'wb'))
+
+print(words)
